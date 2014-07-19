8a273263dac7ec8293f4eef30bc23cf652793422
you21979 <you2197901@gmail.com>
2014-07-19 17:13:37 +0900

    add done

---
+++ promise/done.js
@@ -0,0 +1,29 @@
+"use strict"
+var Promise = require('bluebird');
+var domain = require('domain');
+
+var d = domain.create();
+
+d.on('error', function(err){
+    console.log('domain');
+});
+d.run(function(){
+    Promise.resolve("no json")
+    .then(function (data) {
+        console.log('then1 a');
+        JSON.parse(data);
+        console.log('then1 b');
+    })
+    .then(function (data) {
+        console.log('then2');
+    })
+    .catch(function (data) {
+        console.log('catch');
+    })
+    .done(function (data) {
+        console.log('done a');
+        JSON.parse(data);
+        console.log('done b');
+    });
+
+});
